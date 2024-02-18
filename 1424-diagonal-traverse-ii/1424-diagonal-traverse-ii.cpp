9500db379dc0bd87d66a711b742534d7669a98cb
MuhammadIktear <150543855+MuhammadIktear@users.noreply.github.com>
2024-02-18 18:54:10 +0600

    Time: 187 ms (58.65%), Space: 101 MB (32.44%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        int maxSum=0;
+        vector<int>ans;
+        unordered_map<int,vector<int>>mp;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            for(int j=0;j<nums[i].size();j++)
+            {
+                maxSum=max(maxSum,i+j);
+                {
+                    mp[i+j].push_back(nums[i][j]);
+                }
+            }
+        }
+        for(int i=0;i<=maxSum;i++)
+        {
+            for(auto u:mp[i])
+            {
+                ans.push_back(u);
+            }
+        }
+        return ans;
+    }
+};
