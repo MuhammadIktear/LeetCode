5fc38ff449cd0818facb933b55f7bc800f70955e
MuhammadIktear <150543855+MuhammadIktear@users.noreply.github.com>
2024-04-05 01:03:22 +0600

    Time: 4 ms (45.33%), Space: 15.2 MB (50.58%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+   void fun(vector<string>&ans,int n,int open,int close,string curr_str)
+    {
+        if(curr_str.size()==n*2)
+        {
+            ans.push_back(curr_str);
+            return;
+        }
+        if(open<n)fun(ans,n,open+1,close,curr_str+"(");
+        if(close<open)fun(ans,n,open,close+1,curr_str+")");
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        fun(ans,n,0,0,"");
+        return ans;
+    }
+};
