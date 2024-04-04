da003ccd789b8c7a638c5769e3665197b6062542
MuhammadIktear <150543855+MuhammadIktear@users.noreply.github.com>
2024-04-05 03:29:40 +0600

    Time: 39 ms (83.58%), Space: 50.8 MB (81.82%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,7 +3,7 @@ public:
     bool canJump(vector<int>& nums) {
     int finalPosition = nums.size()- 1;
     for (int idx = nums.size() - 2; idx >= 0; idx--) {
-      if (idx + nums[idx] >= finalPosition) {
+      if (finalPosition-idx<=nums[idx]) {
         finalPosition = idx;
       }
     }
